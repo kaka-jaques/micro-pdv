9a025d08a7b16cf82dffadf6a45f4d3a48dd879c
Kalil J. Fakhouri <53912803+kaka-jaques@users.noreply.github.com>
2022-11-21 18:30:28 +0000

    UPDATE
    NEW COMPONENTS!

--- main-pdv/src/app/app.module.ts
+++ main-pdv/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
